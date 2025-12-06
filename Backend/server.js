4d53af3f3338400023d369fc0f550453285622fa
Abhay Kumar Singh <aksinghcs006@gmail.com>
2025-12-06 12:55:18 +0530

    Enable secure cookies in session configuration

--- Backend/server.js
+++ Backend/server.js
@@ -53,6 +53,7 @@ app.use(
         resave: false,
         saveUninitialized: true,
         cookie: {
+            secure: true,
             httpOnly: true,
             maxAge: 24 * 60 * 60 * 1000
         },
